beae185960658cf359395b6f3b1b1e21e4b758f2
Jack <js99564p@pace.edu>
2026-05-13 16:02:41 -0400

    Updated feature project cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,26 +10,45 @@ import { FeaturedCard } from '../components/FeaturedCard.jsx'
 const FEATURED_PROJECTS = [
   {
     "name" : "Cataclysm",
-    "languages" : "C/C++, C#, OpenGL, GLSL",
+    "languages" : ["C", "C++", "C#", "OpenGL", "GLSL"],
     "description" : "2D rendering & game development engine powered by OpenGL",
     "image" : "assets/images/Cataclysm.png",
     "link" : "https://www.github.com/jackpsmith-git/Cataclysm"
   },
   {
     "name" : "Shader Sandbox",
-    "languages" : "C/C++, OpenGL, GLSL",
+    "languages" : ["C", "C++", "OpenGL", "GLSL"],
     "description" : "Portable Windows tool for testing GLSL shaders",
     "image" : "assets/images/ShaderSandbox.png",
     "link" : "https://www.github.com/jackpsmith-git/ShaderSandbox"
   },
   {
     "name" : "Point-Cloud Renderer",
-    "languages" : "C/C++, Vulkan, GLSL",
+    "languages" : ["C", "C++", "Vulkan", "GLSL"],
     "description" : "3D, point-cloud, Vulkan renderer",
     "image" : "assets/images/PointCloudRenderer.png",
     "link" : "https://www.github.com/jackpsmith-git/PointCloudRenderer"
   },
 ]
+
+const LANG_COLS = {
+  "C": '#064090',
+  "Objective-C" : '#064090',
+  "C++" : '#041e42',
+  "Objective-C++" : '#041e42',
+  "C#": '#68217A',
+  "JavaScript": 'darkorange',
+  "Python": '#0B6623',
+  "Lua": '#6e6a39',
+  "Batchfile" : "#000000",
+  "CMake" : 'green',
+  "HTML" : '#E34C26',
+  "CSS" : '#663399',
+  "Java" : '#007396',
+  "Vulkan" : '#E34C26',
+  "OpenGL" : '#0B6623',
+  "GLSL" : '#25ad49'
+};
 
 function Model(props) {
   const { scene } = useGLTF("assets/scenes/scene.gltf");
@@ -48,7 +67,20 @@ return (
         <div className="flex-col" key={name}>
           <FeaturedCard href={link}>
             <img src={image} alt={name} width="100%" />
-            <h3 className="featured-card-header">{name} ({languages})</h3>
+            <h3 className="featured-card-header">{name}</h3>
+            <span className="languages-span">
+              {languages.slice(0, 12).map((lang) => (
+                <span
+                  key={lang}
+                  className="language"
+                  style={{
+                    backgroundColor: LANG_COLS[lang] || "#000000",
+                  }}
+                >
+                  {lang}
+                </span>
+              ))}
+            </span>
             <hr />
             <p className="featured-card-description">{description}</p>
           </FeaturedCard>
