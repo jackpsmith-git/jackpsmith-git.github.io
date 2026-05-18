92ba05d18ecb707c8854960d88f08cc50af154fc
Jack <jpsmith8603@gmail.com>
2026-05-18 12:29:47 -0400

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/jackpsmith-git.github.io/',
+    base: '/',
 
     build: {
         outDir: 'dist',
