294438c3f709a67f8305a532aa472d49452e93a1
Jack <jpsmith8603@gmail.com>
2026-05-27 14:10:59 -0400

    Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import { Home } from './sections/Home.jsx'
 import { Projects } from './sections/Projects.jsx'
 import { Footer } from './components/Footer.jsx'
 import { ThemeToggle } from './components/ThemeToggle.jsx'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 export default function App() {
 
@@ -20,9 +22,11 @@ export default function App() {
     return (
       <div className="relative z-10 min-h-screen dark:bg-black bg-white transition-colors duration-700 ease-in-out">
         <div ref={container} className="relative">
+          <Analytics />
+          <SpeedInsights />
           <Navbar />
           <Hero />
-          <div className='bg-gradient-to-b from-white dark:from-black to-purple-950/80 dark:to-purple-950/60 bg-contain bg-center transition-colors duration-700 ease-in-out'>
+          <div className='bg-linear-to-b from-white dark:from-black to-purple-950/80 dark:to-purple-950/60 bg-contain bg-center transition-colors duration-700 ease-in-out'>
             <Projects />
             <Home />
           </div>
